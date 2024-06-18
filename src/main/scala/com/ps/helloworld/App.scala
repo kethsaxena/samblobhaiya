927857e8d3a79bbffe3c8b038802744e2e3dfb13
Praketa <praketasaxena@gmail.com>
2024-06-17 20:44:45 -0400

    Initial Commit

---
+++ src/main/scala/com/ps/helloworld/App.scala
@@ -0,0 +1,7 @@
+package com.example.helloworld
+
+object App {
+  def main(args: Array[String]): Unit = {
+    println("Hello, World!")
+  }
+}
---
+++ src/test/scala/com/ps/helloworld/AppTest.scala
@@ -0,0 +1,9 @@
+package com.example.helloworld
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class AppTest extends AnyFunSuite {
+  test("Example test") {
+    assert(1 == 1)
+  }
+}
